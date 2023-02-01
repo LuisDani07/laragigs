87adbf484ce060c9b0762eb90b64b06dd8568385
LuisDani07 <tics270118@gmail.com>
2023-01-31 20:34:54 -0600

    fijar valores para que permanezcan después de un error al subir formulario

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -18,7 +18,7 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="company" />
+                                name="company" value="{{old('company')}}"/>
                              @error('company')
                              <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                              @enderror
@@ -31,7 +31,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="title"
-                                placeholder="Example: Senior Laravel Developer" />
+                                placeholder="Example: Senior Laravel Developer" value="{{old('title')}}"/>
                                 @error('title')
                              <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                              @enderror
@@ -47,7 +47,8 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="location"
-                                placeholder="Example: Remote, Boston MA, etc" />
+                                placeholder="Example: Remote, Boston MA, etc" 
+                                value="{{old('location')}}"/>
                                 @error('location')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                                 @enderror
@@ -60,7 +61,8 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="email"/>
+                                name="email"
+                                value="{{old('email')}}"/>
                                   @error('email')
                              <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                              @enderror
@@ -76,7 +78,7 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="website"/>
+                                name="website" value="{{old('website')}}"/>
                                 @error('website')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                                 @enderror
@@ -90,7 +92,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="tags"
-                                placeholder="Example: Laravel, Backend, Postgres, etc"/>
+                                placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}"/>
                                 @error('tags')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                                 @enderror
@@ -118,7 +120,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="description"
                                 rows="10"
-                                placeholder="Include tasks, requirements, salary, etc"></textarea>
+                                placeholder="Include tasks, requirements, salary, etc" >{{old('description')}}</textarea>
                                 @error('description')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                                 @enderror
