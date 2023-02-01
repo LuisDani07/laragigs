d1e9d3f5cd0c994c11c19526a404ec9833eb542d
LuisDani07 <tics270118@gmail.com>
2023-01-31 19:46:19 -0600

    implementación de mensaje con alpino js para qe desaparzca después de unos segundos

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,5 @@
 @if(Session()->has('message'))
-<div class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
+<div x-data="{show: true}" x-init="setTimeout(()=>show=false, 4000)" x-show="show" class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
 <p>{{Session('message')}}</p>
 </div>
 @endif
