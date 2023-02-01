192490b3e5f6b891de595742b0b469b4587e07ba
LuisDani07 <tics270118@gmail.com>
2023-02-01 15:29:46 -0600

    crear usuarios con contraseña hasheada

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -35,3 +36,9 @@ Route::delete('listings/{listing}', [ListingController::class, 'destroy']);
 
 //single listing
 Route::get('/listings/{listing}',[ListingController::class,'show']);
+
+//show register/create form
+Route::get('/register',[UserController::class, 'create']);
+
+//create users
+Route::post('/users',[UserController::class,'store']);
