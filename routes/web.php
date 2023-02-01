57c44cfe55bcbbf77ca6c8e4b3b51bcf9fc390d5
LuisDani07 <tics270118@gmail.com>
2023-01-31 18:39:56 -0600

    formulario creado para postular empleos

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 //all listing
 Route::get('/', [ListingController::class, 'index']);
+
+//show create form
+Route::get('/listings/create',[ListingController::class,'create']);
+
+
+//store listing data
+Route::post('/listings',[ListingController::class,'store']);
+
 //single listing
-Route::get('/listing/{listing}',[ListingController::class,'show']);
+Route::get('/listings/{listing}',[ListingController::class,'show']);
